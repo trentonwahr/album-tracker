7b0a36418f860257aa3759d085ea44994edbc06a
trentonwahr <trentwahr@gmail.com>
2023-04-23 15:34:25 -0400

    add profile funcitonality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,9 +1,19 @@
 import { Profile } from '../models/profile.js'
 
+
 function show(req, res) {
-  Profile.findById(req.params.albumId)
+  Profile.findById(req.params.profileId)
   .then(profile => {
     const isSelf = profile._id.equals(req.user.profile._id)
+    res.render('profiles/show', {
+      title: 'My Profile',
+      profile,
+      isSelf
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+    res.redirect('/albums')
   })
 }
 
--- routes/profiles.js
+++ routes/profiles.js
@@ -4,7 +4,7 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('/:albumId', isLoggedIn, profilesCtrl.show)
+router.get('/:profileId', isLoggedIn, profilesCtrl.show)
 
 export {
   router
