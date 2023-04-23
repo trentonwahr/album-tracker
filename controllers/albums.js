be0305f3dd7c1e1976cb20a6c22c96e22d8db5f8
trentonwahr <trentwahr@gmail.com>
2023-04-23 14:39:31 -0400

    add album CRU

---
+++ controllers/albums.js
@@ -0,0 +1,80 @@
+import { Album } from "../models/album.js"
+
+function index(req, res) {
+  Album.find({})
+  .then(albums => {
+    res.render('albums/index', {
+      albums,
+      title: 'All Albums',
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function newAlbum(req, res) {
+  res.render('albums/new', {
+    title: 'Add Album',
+  })
+}
+
+function create(req, res) {
+  Album.create(req.body)
+  .then(album => {
+    res.redirect(`/albums/${album._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/albums/new')
+  })
+}
+
+function show(req, res) {
+  Album.findById(req.params.albumId)
+  .then(album => {
+    res.render('albums/show', {
+      album,
+      title: "Album Details",
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/albums')
+  })
+}
+
+function edit(req, res) {
+  Album.findById(req.params.albumId)
+  .then(album => {
+    res.render('albums/edit', {
+      album,
+      title: 'Edit Album',
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/albums')
+  })
+}
+
+function update(req, res) {
+  Album.findByIdAndUpdate(req.params.albumId, req.body, {new: true})
+  .then(album => {
+    res.redirect(`/albums/${album._id}`)
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/albums')
+  })
+}
+
+export {
+  index,
+  newAlbum as new,
+  create,
+  show,
+  edit,
+  update,
+}
--- routes/albums.js
+++ routes/albums.js
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 import * as albumsCtrl from '../controllers/albums.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('')
+router.get('/', albumsCtrl.index)
+router.get('/new', albumsCtrl.new)
+router.get('/:albumId', albumsCtrl.show)
+router.get('/:albumId/edit', albumsCtrl.edit)
+router.post('/', albumsCtrl.create)
+router.put('/:albumId', albumsCtrl.update)
 
 export {
   router
